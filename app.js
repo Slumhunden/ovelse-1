c2e888d3603011d7d574ae8ea1e5c06e0a50e7ae
Slumhunden <kroghbastin@gmail.com>
2023-01-31 11:58:48 +0100

    button

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 document.body.innerHTML += "Hello Javascript!";
 // alert("Hello Dat.js);");
-console.log("Hello Dat.js");
-console.log(" Buenos Dias Studentos");
-console.log("Los codos maximus");
+//console.log("Hello Dat.js");
+//console.log(" Buenos Dias Studentos");
+//console.log("Los codos maximus");
+
+
+function sayHello() {
+    document.querySelector("h3").textContent = "hej " + document.querySelector("input").value
+}
+document.querySelector("button").addEventListener("click",sayHello)
